1fcb95f80b301a9293545816c9c4a913b5f11de5
oanapuscas12 <oana.puscas@verticaldigital.ca>
2024-08-19 15:27:00 +0300

    add-merchants-monthly chart

--- adminPanel/src/main/java/com/ecommerce/controller/ReportsController.java
+++ adminPanel/src/main/java/com/ecommerce/controller/ReportsController.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class ReportsController {
@@ -32,17 +33,35 @@ public class ReportsController {
         }
 
         int currentYear = LocalDate.now().getYear();
-        Map<String, Long> monthlyEnrollments = userService.getMonthlyMerchantEnrollments(currentYear);
 
-        List<String> months = new ArrayList<>(monthlyEnrollments.keySet());
-        List<Long> enrollments = new ArrayList<>(monthlyEnrollments.values());
+        // Data for Enrollment Merchants/Months Overview
+        Map<String, Long> monthlyMerchantsEnrollments = userService.getAllMonthlyMerchantEnrollments(currentYear);
+
+        List<String> monthsForMerchants = new ArrayList<>(monthlyMerchantsEnrollments.keySet());
+        List<Long> enrollmentsForMerchants = new ArrayList<>(monthlyMerchantsEnrollments.values());
 
         ObjectMapper mapper = new ObjectMapper();
-        String monthsJson = mapper.writeValueAsString(months);
-        String enrollmentsJson = mapper.writeValueAsString(enrollments);
+        String monthsForMerchantsJson = mapper.writeValueAsString(monthsForMerchants);
+        String merchantsEnrollmentsForMerchantsJson = mapper.writeValueAsString(enrollmentsForMerchants);
 
-        model.addAttribute("monthsJson", monthsJson);
-        model.addAttribute("enrollmentsJson", enrollmentsJson);
+        model.addAttribute("monthsForMerchantsJson", monthsForMerchantsJson);
+        model.addAttribute("merchantsEnrollmentsForMerchantsJson", merchantsEnrollmentsForMerchantsJson);
+
+        // Data for Enrollment Admins/Months Overview
+        Map<String, Long> monthlyAdminsEnrollments = userService.getAllMonthlyAdminEnrollmentPercentages(currentYear);
+
+        List<String> monthsForAdmins = new ArrayList<>(monthlyAdminsEnrollments.keySet());
+        List<Long> enrollmentsForAdmins = new ArrayList<>(monthlyAdminsEnrollments.values());
+
+        String monthsForAdminsJson = mapper.writeValueAsString(monthsForAdmins);
+        System.out.println(monthsForAdminsJson);
+        String merchantsEnrollmentsForAdminsJson = mapper.writeValueAsString(enrollmentsForAdmins);
+        System.out.println(merchantsEnrollmentsForAdminsJson);
+
+
+        model.addAttribute("monthsForAdminsJson", monthsForAdminsJson);
+        model.addAttribute("merchantsEnrollmentsForAdminsJson", merchantsEnrollmentsForAdminsJson);
+
         model.addAttribute("role", pageRole);
         model.addAttribute("otherRole", otherRole);
         model.addAttribute("pageTitle", "View Reports");
