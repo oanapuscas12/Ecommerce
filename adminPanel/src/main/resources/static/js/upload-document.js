7ee91ecbb35cdabc62a7f741cda3b8a9c60816af
oanapuscas12 <oana.puscas@verticaldigital.ca>
2024-08-22 11:35:48 +0300

    redesign upload-document.html

--- adminPanel/src/main/resources/static/js/upload-document.js
+++ adminPanel/src/main/resources/static/js/upload-document.js
@@ -1,16 +1,45 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const errorMessageElement = document.getElementById('error-message');
-    const successMessageElement = document.getElementById('success-message');
+    document.addEventListener("DOMContentLoaded", function() {
+        const errorMessageElement = document.getElementById('error-message');
+        const successMessageElement = document.getElementById('success-message');
+        const userId = document.getElementById('userId');
+        const fileInput = document.getElementById('fileInput');
+        const submitButton = document.getElementById('submitButton');
+        const isAdmin = document.querySelector('form').hasAttribute('data-is-admin'); // Determine if admin
 
-    if (errorMessageElement) {
-        setTimeout(() => {
-            errorMessageElement.style.display = 'none';
-        }, 3000);
-    }
+        function hideMessages() {
+            if (errorMessageElement) {
+                setTimeout(() => {
+                    errorMessageElement.style.display = 'none';
+                }, 3000);
+            }
 
-    if (successMessageElement) {
-        setTimeout(() => {
-            successMessageElement.style.display = 'none';
-        }, 3000);
-    }
-});
+            if (successMessageElement) {
+                setTimeout(() => {
+                    successMessageElement.style.display = 'none';
+                }, 3000);
+            }
+        }
+
+        function checkFormValidity() {
+            let isValid;
+
+            if (isAdmin) {
+                isValid = userId.value !== "" && fileInput.files.length > 0;
+            } else {
+                isValid = fileInput.files.length > 0;
+            }
+
+            const hasError = errorMessageElement && errorMessageElement.style.display !== 'none';
+            submitButton.disabled = !isValid || hasError;
+            submitButton.style.pointerEvents = submitButton.disabled ? 'none' : 'auto';
+        }
+
+        checkFormValidity();
+
+        if (userId) {
+            userId.addEventListener('change', checkFormValidity);
+        }
+        fileInput.addEventListener('change', checkFormValidity);
+
+        hideMessages();
+    });
