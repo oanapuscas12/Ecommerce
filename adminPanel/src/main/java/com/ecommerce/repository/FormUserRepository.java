cdddd879ae3c4dc41094146e5f4af64dec00da4c
oanapuscas12 <oana.puscas@verticaldigital.ca>
2024-08-26 11:11:11 +0300

    deleted form-users

--- adminPanel/src/main/java/com/ecommerce/repository/FormUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.repository;
-
-import com.ecommerce.model.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormUserRepository extends JpaRepository<Merchant, Long> {
-}
